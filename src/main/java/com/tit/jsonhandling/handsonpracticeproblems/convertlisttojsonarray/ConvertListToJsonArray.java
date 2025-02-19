926b3fb7e842fa60cd7a70fb5ea55ec6bf27a1fe
aadarsh <aadarshg2562003@example.com>
2025-02-19 16:46:55 +0530

    [Aadarsh] Add . Convert list of objects to JSON array

---
+++ src/main/java/com/tit/jsonhandling/handsonpracticeproblems/convertlisttojsonarray/ConvertListToJsonArray.java
@@ -0,0 +1,26 @@
+package com.tit.jsonhandling.handsonpracticeproblems.convertlisttojsonarray;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tit.jsonhandling.practiceproblems.convertlisttojsonarray.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertListToJsonArray {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Alice" ,21));
+        users.add(new User("Brook" ,22));
+        users.add(new User("Reo" ,25));
+        users.add(new User("Nami" ,18));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String usersJson = objectMapper.writeValueAsString(users);
+            System.out.println(usersJson);
+        } catch (Exception e) {
+            System.out.println("Invalid Json!");
+        }
+
+    }
+}

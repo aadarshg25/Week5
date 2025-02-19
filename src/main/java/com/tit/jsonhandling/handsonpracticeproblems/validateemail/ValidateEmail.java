99b498f1c5d839b826753f9889034b61207f8a42
aadarsh <aadarshg2562003@example.com>
2025-02-19 21:44:09 +0530

    [Aadarsh] Add . Validate email field using JSON Schema

---
+++ src/main/java/com/tit/jsonhandling/handsonpracticeproblems/validateemail/ValidateEmail.java
@@ -0,0 +1,29 @@
+package com.tit.jsonhandling.handsonpracticeproblems.validateemail;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import java.io.FileInputStream;
+
+public class ValidateEmail {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Schema schema = SchemaLoader.load(new JSONObject(new JSONTokener(new FileInputStream("C:\\Users\\ASUS\\Desktop\\Day02_JsonHandling\\src\\main\\resources\\email_schema.json"))));
+
+            schema.validate(new JSONObject(new JSONTokener(new FileInputStream("C:\\Users\\ASUS\\Desktop\\Day02_JsonHandling\\src\\main\\resources\\email.json"))));
+
+            System.out.println("All emails are valid!");
+
+        } catch (Exception e) {
+
+            System.out.println("Validation failed: " + e.getMessage());
+
+        }
+
+    }
+
+}
